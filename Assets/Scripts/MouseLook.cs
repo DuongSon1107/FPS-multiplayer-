c602f2638c8c95acbcd785ca4ad86e2d221a4cd8
Triệu Dương Sơn <98730673+DuongSon1107@users.noreply.github.com>
2024-01-31 20:53:11 +0700

    first commit

---
+++ Assets/Scripts/MouseLook.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public static MouseLook instance;
+
+    [Header("Settings")]
+    public Vector2 clampInDegrees = new Vector2(360, 180);
+    public bool lockCursor = true;
+    [Space]
+    private Vector2 sensitivity = new Vector2(2, 2);
+    [Space]
+    public Vector2 smoothing = new Vector2(3, 3);
+
+    [Header("First Person")]
+    public GameObject characterBody;
+
+    private Vector2 targetDirection;
+    private Vector2 targetCharacterDirection;
+
+    private Vector2 _mouseAbsolute;
+    private Vector2 _smoothMouse;
+
+    private Vector2 mouseDelta;
+
+    [HideInInspector]
+    public bool scoped;
+
+    void Start()
+    {
+        instance = this;
+
+        // Set target direction to the camera's initial orientation.
+        targetDirection = transform.localRotation.eulerAngles;
+
+        // Set target direction for the character body to its inital state.
+        if (characterBody)
+            targetCharacterDirection = characterBody.transform.localRotation.eulerAngles;
+        
+        if (lockCursor)
+            LockCursor();
+
+    }
+
+    public void LockCursor()
+    {
+        // make the cursor hidden and locked
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    void Update()
+    {
+        // Góc quay X và Y - góc quay cuối cùng + góc quay người chơi
+        var targetOrientation = Quaternion.Euler(targetDirection);
+        var targetCharacterOrientation = Quaternion.Euler(targetCharacterDirection);
+
+        // Giá trị con chuột lấy ngay tại thời điểm hiện tại
+        mouseDelta = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+
+        // Scale giá trị con chuột với giá trị Vector2 
+        mouseDelta = Vector2.Scale(mouseDelta, new Vector2(sensitivity.x * smoothing.x, sensitivity.y * smoothing.y));
+
+        // Nội suy tuyến tính, giúp con chuột mượt hơn
+        _smoothMouse.x = Mathf.Lerp(_smoothMouse.x, mouseDelta.x, 1f / smoothing.x);
+        _smoothMouse.y = Mathf.Lerp(_smoothMouse.y, mouseDelta.y, 1f / smoothing.y);
+
+        // Cập nhật góc quay tuyệt đối  
+        _mouseAbsolute += _smoothMouse;
+
+        // Clamp góc quay
+        if (clampInDegrees.x < 360)
+            _mouseAbsolute.x = Mathf.Clamp(_mouseAbsolute.x, -clampInDegrees.x * 0.5f, clampInDegrees.x * 0.5f);
+
+        if (clampInDegrees.y < 360)
+            _mouseAbsolute.y = Mathf.Clamp(_mouseAbsolute.y, -clampInDegrees.y * 0.5f, clampInDegrees.y * 0.5f);
+
+        transform.localRotation = Quaternion.AngleAxis(-_mouseAbsolute.y, targetOrientation * Vector3.right) * targetOrientation;
+
+        // Áp dụng góc quay cho camera của nhân vật
+        if (characterBody)
+        {
+            // Tạo góc quay quanh một trục  
+            var yRotation = Quaternion.AngleAxis(_mouseAbsolute.x, Vector3.up);
+            characterBody.transform.localRotation = yRotation * targetCharacterOrientation;
+        }
+        else
+        {
+            var yRotation = Quaternion.AngleAxis(_mouseAbsolute.x, transform.InverseTransformDirection(Vector3.up));
+            transform.localRotation *= yRotation;
+        }
+    }
+}
---
+++ Assets/Scripts/WeaponSwitcher.cs
@@ -0,0 +1,100 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponSwitcher : MonoBehaviour
+{
+    public PhotonView playerSetupView;
+
+    private int selectedWeapon = 0;
+
+    public Animation _animation;
+    public AnimationClip draw;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int previousSelectedWeapon = selectedWeapon;
+
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            selectedWeapon = 0;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            selectedWeapon = 1;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            selectedWeapon = 2;
+        }
+
+        if(Input.GetAxis("Mouse ScrollWheel") > 0)
+        {
+            if(selectedWeapon >= transform.childCount - 1)
+            {
+                selectedWeapon = 0;
+            }
+            else
+            {
+                selectedWeapon += 1;
+            }
+        }
+
+        if (Input.GetAxis("Mouse ScrollWheel") < 0)
+        {
+            if (selectedWeapon <= 0)
+            {
+                selectedWeapon = transform.childCount - 1;
+            }
+            else
+            {
+                selectedWeapon -= 1;
+            }
+        }
+        // Nếu vũ khí đã chuyển đổi, gọi hàm SelectWeapon
+        if (previousSelectedWeapon != selectedWeapon)
+        {
+            SelectWeapon();
+        }
+    }
+
+    void SelectWeapon()
+    {
+        // Sử dụng Remote Procedure Call (RPC) để đặt vũ khí được chọn cho tất cả người chơi
+        playerSetupView.RPC("SetTPWeapon", RpcTarget.All, selectedWeapon);
+        // Đảm bảo rằng selectedWeapon không vượt quá số lượng vũ khí có sẵn
+        if (selectedWeapon >= transform.childCount)
+        {
+            selectedWeapon = transform.childCount - 1;
+        }
+
+        _animation.Stop();
+        _animation.Play(draw.name);
+
+        // Duyệt và hiển thị vũ khí được chọn, ẩn các vũ khí khác
+        int i = 0;
+        foreach (Transform _weapon in transform)
+        {
+            if(i == selectedWeapon)
+            {
+                _weapon.gameObject.SetActive(true);
+            }
+            else
+            {
+                _weapon.gameObject.SetActive(false);
+            }
+
+            i++;
+        }
+    }
+}
